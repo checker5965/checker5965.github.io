fc110ac9d1246e05484acc5589e75cbdbcde1ae8
checker5965 <archit.checker5@gmail.com>
2018-05-02 23:47:52 +0530

    Progress bar working

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  var current_progress = 0;
+  var interval = setInterval(function() {
+      current_progress += 1;
+      $("#myBar")
+      .css("width", current_progress + "%")
+      .attr("aria-valuenow", current_progress)
+      if (current_progress >= 80)
+          clearInterval(interval);
+  }, 10);
+});
