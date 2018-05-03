830594da253cb75bfa59b09db5a34f211e9a1749
checker5965 <archit.checker5@gmail.com>
2018-05-03 19:32:40 +0530

    changes in javascript, css

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+$(window).scroll( function(){
+  
+      /* Check the location of each desired element */
+      $("p").each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},2000);
+                  
+          }
+          
+      }); 
+  
+});
+$(window).scroll( function(){
+  
+  /* Check the location of each desired element */
+  $(".pr").each( function(i){
+      
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+          
+          $(this).animate({'opacity':'1'},2000);
+              
+      }
+      
+  }); 
+
+});
 $(function() {
   var current_progress = 0;
   var interval = setInterval(function() {
