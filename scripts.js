70117c29f3af13ae10a539344529ff3112b7c196
checker5965 <archit.checker5@gmail.com>
2018-05-05 05:00:50 +0530

    Added CV

--- scripts.js
+++ scripts.js
@@ -9,12 +9,9 @@ $(window).scroll( function(){
           /* If the object is completely visible in the window, fade it it */
           if( bottom_of_window > bottom_of_object ){
               
-              $(this).animate({'opacity':'1'},2000);
-                  
+              $(this).animate({'opacity':'1'},2000);       
           }
-          
       }); 
-  
 });
 $(window).scroll( function(){
   
